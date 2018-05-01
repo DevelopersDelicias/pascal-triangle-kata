3c6015f60a1974d98591f2032175cb9a0cc46164
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-05-01 12:50:47 -0600

    Small refactor

--- src/main/java/com/developersdelicias/PascalTriangle.java
+++ src/main/java/com/developersdelicias/PascalTriangle.java
@@ -9,39 +9,51 @@ public class PascalTriangle {
 	}
 
 	public Levels levels() {
+		Level[] levels = new Level[level];
 		if (this.level == 1)
-			return new Levels(
-					new Level(
-							new Value(1)
-					)
-			);
+			levels[0] = levelOne();
 
 		if (this.level == 2) {
-			return new Levels(
-					new Level(
-							new Value(1)
-					),
-					new Level(
-							new Value(1),
-							new Value(1)
-					)
-			);
+			levels[0] = levelOne();
+			levels[1] = levelTwo();
 		}
 
-		return new Levels(
-				new Level(
-						new Value(1)
-				),
-				new Level(
-						new Value(1),
-						new Value(1)
-				),
-				new Level(
-						new Value(1),
-						new Value(2),
-						new Value(1)
+		if (this.level == 3) {
+			levels[0] = levelOne();
+			levels[1] = levelTwo();
+			levels[2] = levelThree();
+		}
 
-				)
+		return new Levels(levels);
+	}
+
+	private Level levelThree() {
+		return new Level(
+				valueOfOne(),
+				valueOfTwo(),
+				valueOfOne()
+
+		);
+	}
+
+	private Value valueOfTwo() {
+		return new Value(2);
+	}
+
+	private Value valueOfOne() {
+		return new Value(1);
+	}
+
+	private Level levelTwo() {
+		return new Level(
+				valueOfOne(),
+				valueOfOne()
+		);
+	}
+
+	private Level levelOne() {
+		return new Level(
+				valueOfOne()
 		);
 	}
 
