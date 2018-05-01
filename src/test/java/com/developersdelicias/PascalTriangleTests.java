3e5e5921ea2b59b16d21acac35ee182437f1318c
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-05-01 12:24:16 -0600

    Add new test but ignored for now

--- src/test/java/com/developersdelicias/PascalTriangleTests.java
+++ src/test/java/com/developersdelicias/PascalTriangleTests.java
@@ -1,11 +1,15 @@
 package com.developersdelicias;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class PascalTriangleTests {
+
+	private static final int ANY_LEVEL = 2;
 
 	@Test
 	public void is_a_collection_of_levels_level_one() {
@@ -66,6 +70,29 @@ public class PascalTriangleTests {
 								)
 						)
 				)
+		);
+	}
+
+	@Test
+	@Ignore("Not sure if I want to implement this logic like this")
+	public void can_be_represented_as_a_list_of_levels_for_single_level() {
+		PascalTriangle pascalTriangle = Mockito.spy(new PascalTriangle(ANY_LEVEL));
+
+		Levels levels = new Levels(
+				new Level(
+						new Value(1),
+						new Value(2),
+						new Value(3),
+						new Value(4)
+				)
+		);
+
+		Mockito.doReturn(levels).when(pascalTriangle).levels();
+
+		assertThat(
+				"Cannot represent one single level Pascal Triangle as String",
+				pascalTriangle.toString(),
+				is("[1, 2, 3, 4]")
 		);
 	}
 }
