c7f404360620ce77a222345199f381b723aeeb23
Benjamin Cisneros Barraza <cisnerosbarraza@gmail>
2023-09-24 15:07:13 -0600

    Improve tests

--- src/test/java/com/developersdelicias/katas/pascaltriangle/AcceptanceTest.java
+++ src/test/java/com/developersdelicias/katas/pascaltriangle/AcceptanceTest.java
@@ -68,34 +68,15 @@ public class AcceptanceTest {
 	}
 
 	@Test
-	public void triangle_of_11() {
+	public void triangle_of_11() throws Exception{
 		format = new BranchedPascalTriangleFormat();
 
 		printPascalTriangleOfLevel(11);
 
-		verify(output).print(
-						"                    1\n" +
-						"                   / \\\n" +
-						"                  1   1\n" +
-						"                 / \\ / \\\n" +
-						"                1   2   1\n" +
-						"               / \\ / \\ / \\\n" +
-						"              1   3   3   1\n" +
-						"             / \\ / \\ / \\ / \\\n" +
-						"            1   4   6   4   1\n" +
-						"           / \\ / \\ / \\ / \\ / \\\n" +
-						"          1   5  10  10   5   1\n" +
-						"         / \\ / \\ / \\ / \\ / \\ / \\\n" +
-						"        1   6  15  20  15   6   1\n" +
-						"       / \\ / \\ / \\ / \\ / \\ / \\ / \\\n" +
-						"      1   7  21  35  35  21   7   1\n" +
-						"     / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\\n" +
-						"    1   8  28  56  70  56  28   8   1\n" +
-						"   / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\\n" +
-						"  1   9  36  84  126 126 84  36   9   1\n" +
-						" / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\\n" +
-						"1  10  45  120 210 252 210 120 45  10   1"
-		);
+		String expectedTriangle = fromFile("src/test/resources/pascal_triangle_level_11.txt");
+
+
+		verify(output).print(expectedTriangle);
 	}
 
 	@Test
